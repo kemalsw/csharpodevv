232d8fb70c80f42e1a768cb3779585fb7ee89b9c
Ali Kemal Gündüz <kemall.gunduz@icloud.com>
2023-07-04 16:42:54 +0300

    ikincikomut

--- csharpodeev/Program.cs
+++ csharpodeev/Program.cs
@@ -10,6 +10,21 @@ namespace csharpodeev
     {
         static void Main(string[] args)
         {
+            byte b = 5;
+            sbyte c = 5;
+
+            short s = 5;
+            ushort us = 5;
+            Int16 i16 = 5;
+            int i = 5;
+            Int32 i32 = 5;
+            Int64 i64 = 2;
+
+            uint ui = 2;
+            long l = 4;
+            ulong ul = 3;
+
+
         }
     }
 }
