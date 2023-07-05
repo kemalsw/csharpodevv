92c36b9bae1e8f16997b2aa10ec9b8d1640cd51a
Ali Kemal Gündüz <kemall.gunduz@icloud.com>
2023-07-05 11:32:33 +0300

    secondcommit

--- csharpodeev/Program.cs
+++ csharpodeev/Program.cs
@@ -10,21 +10,20 @@ namespace csharpodeev
     {
         static void Main(string[] args)
         {
-            byte b = 5;
-            sbyte c = 5;
-
-            short s = 5;
-            ushort us = 5;
-            Int16 i16 = 5;
-            int i = 5;
-            Int32 i32 = 5;
-            Int64 i64 = 2;
-
-            uint ui = 2;
-            long l = 4;
-            ulong ul = 3;
+            int x = 3;
+            int y = 3;
+            y = y + 2;
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y /= 1;
+            Console.WriteLine(y);
+            x += 2;
+            Console.WriteLine(x);
+            Console.ReadLine();
 
 
+             
         }
     }
 }
